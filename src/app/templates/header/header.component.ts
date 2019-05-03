1f25e4efc4f450c7c34c2f5da22fbfa94991aefa
GagaMen <felixdimmel93@gmail.com>
2019-05-04 00:12:05 +0200

    Optimize header component + create basic form component + add some css styles

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -3,7 +3,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material';
 
 @Component({
-  selector: 'app-template-header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
