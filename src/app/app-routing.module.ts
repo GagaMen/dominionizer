3c62fde346d073ca73b55ba201d8a3d9808b8d6d
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2019-05-04 22:37:28 +0200

    Add route for HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  // TODO: Redirect to PageNotFoundComponent when existent
+  {path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
