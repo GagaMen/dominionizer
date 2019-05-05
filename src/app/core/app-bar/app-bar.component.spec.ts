acd5e061c9124bdc44f4c4e82b67082c4a0629be
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2019-05-05 18:26:04 +0200

    Rename HeaderComponent to AppBarComponent

--- src/app/core/app-bar/app-bar.component.spec.ts
+++ src/app/core/app-bar/app-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { AppBarComponent } from './app-bar.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+  let component: AppBarComponent;
+  let fixture: ComponentFixture<AppBarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ AppBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(AppBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
