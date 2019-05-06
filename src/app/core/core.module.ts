48ff96f5721e1f48aede0e5001b27d5c7b32dd1b
Felix Dimmel <12294922+GagaMen@users.noreply.github.com>
2019-05-06 23:46:37 +0200

    Disable count field if switches are unchecked

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,7 +5,7 @@ import { CoreRoutingModule } from './core-routing.module';
 import { AppBarComponent } from './app-bar/app-bar.component';
 import { GenerateSetFormComponent } from './generate-set-form/generate-set-form.component';
 import { MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatSlideToggleModule, MatCardModule, MatDividerModule, MatStepperModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatCardModule,
     MatDividerModule,
     MatStepperModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    FormsModule
   ],
   exports: [
     AppBarComponent,
