a7322ba321390abbf05917821d56c5e31012a566
Felix Dimmel <12294922+GagaMen@users.noreply.github.com>
2019-05-27 22:25:43 +0200

    Add endpoint for cards to data service

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Extension } from '../models/extension';
+import { Card } from '../models/card';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class DataService {
 
   extensions(): Observable<Extension[]> {
     return this.http.get<Extension[]>('/assets/data/extensions.json');
+  }
+
+  cards(): Observable<Card[]> {
+    return this.http.get<Card[]>('/assets/data/cards.json');
   }
 }
