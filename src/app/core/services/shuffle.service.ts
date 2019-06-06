6d64821cb81af898894eb21edbae95af05978ec5
Felix Dimmel <12294922+GagaMen@users.noreply.github.com>
2019-06-06 23:48:21 +0200

    Filter cards by selected extensions + draw randomly

--- src/app/core/services/shuffle.service.ts
+++ src/app/core/services/shuffle.service.ts
@@ -2,19 +2,36 @@ import { Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { Card } from '../models/card';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 import { Configuration } from '../models/configuration';
+import * as math from 'mathjs';
+import { Extension } from '../models/extension';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShuffleService {
+  private configuration: Configuration;
 
   constructor(private dataService: DataService ) { }
 
   shuffle(configuration: Configuration): Observable<Card[]> {
+    this.configuration = configuration;
+
     return this.dataService.cards().pipe(
-      map((cards: Card[]) => cards.slice(0, 9))
+      map((cards: Card[]) => this.filterByExtensions(cards)),
+      map((cards: Card[]) => this.pickRandom(cards))
+    );
+  }
+
+  pickRandom(cards: Card[]): Card[] {
+    const pickedCardIds = math.pickRandom(cards.map((card: Card) => card.id), 10) as any as number[];
+    return pickedCardIds.map((cardId: number) => cards.find((card: Card) => card.id === cardId));
+  }
+
+  filterByExtensions(cards: Card[]): Card[] {
+    return cards.filter((card: Card) =>
+      this.configuration.extensions.map((extension: Extension) => extension.id).includes(card.extension.id)
     );
   }
 }
