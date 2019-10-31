5f78bbeeaaa1fcd179076263ef71a54b6e9bf063
GagaMen <felixdimmel93@gmail.com>
2019-10-31 21:51:20 +0100

    Add event cards generation
    Co-authored-by: Tobias Dimmel <tobi.dimmel@gmail.com>

--- src/app/core/services/shuffle.service.ts
+++ src/app/core/services/shuffle.service.ts
@@ -15,17 +15,26 @@ export class ShuffleService {
 
   constructor(private dataService: DataService ) { }
 
-  shuffle(configuration: Configuration): Observable<Card[]> {
+  shuffleCards(configuration: Configuration): Observable<Card[]> {
     this.configuration = configuration;
 
     return this.dataService.cards().pipe(
       map((cards: Card[]) => this.filterByExtensions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards))
+      map((cards: Card[]) => this.pickRandom(cards, 10))
     );
   }
 
-  pickRandom(cards: Card[]): Card[] {
-    const pickedCardIds = math.pickRandom(cards.map((card: Card) => card.id), 10) as any as number[];
+  shuffelEvents(configuration: Configuration): Observable<Card[]> {
+    this.configuration = configuration;
+
+    return this.dataService.events().pipe(
+      map((cards: Card[]) => this.filterByExtensions(cards)),
+      map((cards: Card[]) => this.pickRandom(cards, configuration.options.eventCount))
+    );
+  }
+
+  pickRandom(cards: Card[], amount: number): Card[] {
+    const pickedCardIds = math.pickRandom(cards.map((card: Card) => card.id), amount) as any as number[];
     return pickedCardIds.map((cardId: number) => cards.find((card: Card) => card.id === cardId));
   }
 
