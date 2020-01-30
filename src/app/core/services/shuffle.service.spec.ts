f619ef063bbd2046ad2d9eb7f4bc1fe43d2fd8f2
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-01-30 21:52:24 +0100

    Use cost distribution to pick random cards
    Co-authored-by: Felix Dimmel <12294922+GagaMen@users.noreply.github.com>

--- src/app/core/services/shuffle.service.spec.ts
+++ src/app/core/services/shuffle.service.spec.ts
@@ -1,12 +1,123 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ShuffleService } from './shuffle.service';
+import { DataService } from './data.service';
+import { ConfigurationService } from './configuration.service';
+import { SpyObj } from 'src/testing/spy-obj';
+import { cold, getTestScheduler } from 'jasmine-marbles';
+import { Configuration } from '../models/configuration';
+import { Expansion } from '../models/expansion';
+import { Card } from '../models/card';
+import { CardType } from '../models/card-type';
+import { MathJsService } from './math-js.service';
 
-xdescribe('ShuffleService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('ShuffleService', () => {
+  let shuffleService: ShuffleService;
+  let dataServiceSpy: SpyObj<DataService>;
+  let configurationServiceSpy: SpyObj<ConfigurationService>;
+  let mathJsServiceSpy: SpyObj<MathJsService>;
 
-  it('should be created', () => {
-    const service: ShuffleService = TestBed.get(ShuffleService);
-    expect(service).toBeTruthy();
+  const defaultTestExpansion: Expansion = {
+    id: 1,
+    name: 'Default Test Expansion',
+  };
+  const firstTestCard: Card = {
+    id: 1,
+    name: 'First Test Card',
+    description: '',
+    expansion: defaultTestExpansion,
+    types: [CardType.Action],
+    cost: 4,
+  };
+  const secondTestCard: Card = {
+    id: 2,
+    name: 'Second Test Card',
+    description: '',
+    expansion: defaultTestExpansion,
+    types: [CardType.Action],
+    cost: 5,
+  };
+  const thirdTestCard: Card = {
+    id: 3,
+    name: 'Third Test Card',
+    description: '',
+    expansion: defaultTestExpansion,
+    types: [CardType.Action],
+    cost: 5,
+  };
+  const defaultConfiguration: Configuration = {
+    expansions: [defaultTestExpansion],
+    options: {
+      events: 0,
+      landmarks: 0,
+      boons: 0,
+      hexes: 0,
+      states: 0,
+    },
+    costDistribution: new Map<number, number>(),
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: DataService,
+          useValue: jasmine.createSpyObj<DataService>('DataService', ['cards']),
+        },
+        {
+          provide: ConfigurationService,
+          useValue: {},
+        },
+        {
+          provide: MathJsService,
+          useValue: jasmine.createSpyObj<MathJsService>('MathJsService', ['pickRandom']),
+        },
+      ]
+    });
+
+    dataServiceSpy = TestBed.get(DataService);
+    dataServiceSpy.cards.and.returnValue(cold('a', { a: [firstTestCard, secondTestCard, thirdTestCard]}));
+    configurationServiceSpy = TestBed.get(ConfigurationService);
+    configurationServiceSpy.configuration$ = cold('a', { a: defaultConfiguration });
+    mathJsServiceSpy = TestBed.get(MathJsService);
+    mathJsServiceSpy.pickRandom.and.returnValue([]);
+  });
+
+  describe('shuffleCards', () => {
+    it('should pass all card ids for selected expansions to MathJsService.pickRandom()', () => {
+      const cardIds = [firstTestCard.id, secondTestCard.id, thirdTestCard.id];
+      shuffleService = TestBed.get(ShuffleService);
+
+      shuffleService.shuffleCards(1).subscribe(() => {}, fail);
+      getTestScheduler().flush();
+
+      expect(mathJsServiceSpy.pickRandom).toHaveBeenCalledWith(cardIds, jasmine.anything(), jasmine.anything());
+    });
+
+    it('should pass amount value to MathJsService.pickRandom()', () => {
+      const amount = 3;
+      shuffleService = TestBed.get(ShuffleService);
+
+      shuffleService.shuffleCards(amount).subscribe(() => {}, fail);
+      getTestScheduler().flush();
+
+      expect(mathJsServiceSpy.pickRandom).toHaveBeenCalledWith(jasmine.anything(), amount, jasmine.anything());
+    });
+
+    it('should pass correct card weights to MathJsService.pickRandom()', () => {
+      const configuration: Configuration = {
+        ...defaultConfiguration,
+        costDistribution: new Map<number, number>([[4, 1], [5, 2]]),
+      };
+      configurationServiceSpy.configuration$ = cold('a', { a: configuration });
+      // calculation formula: cost distribution value / count of cards with equal cost
+      const cardWeights = [1/1, 2/2, 2/2];
+      shuffleService = TestBed.get(ShuffleService);
+
+      shuffleService.shuffleCards(1).subscribe(() => {}, fail);
+      getTestScheduler().flush();
+
+      expect(mathJsServiceSpy.pickRandom).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), cardWeights);
+    });
   });
 });
--- src/app/core/services/shuffle.service.ts
+++ src/app/core/services/shuffle.service.ts
@@ -5,8 +5,8 @@ import { DataService } from './data.service';
 import { Card } from '../models/card';
 import { Observable, of, iif } from 'rxjs';
 import { map } from 'rxjs/operators';
-import * as math from 'mathjs';
 import { Expansion } from '../models/expansion';
+import { MathJsService } from './math-js.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,24 +16,25 @@ export class ShuffleService {
 
   constructor(
     private dataService: DataService,
-    private configurationService: ConfigurationService
+    private configurationService: ConfigurationService,
+    private mathJsService: MathJsService
   ) {
     this.configurationService.configuration$.subscribe(
       (configuration: Configuration) => this.configuration = configuration
     );
   }
 
-  shuffleCards(): Observable<Card[]> {
+  shuffleCards(amount: number = 10): Observable<Card[]> {
     return this.dataService.cards().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, 10))
+      map((cards: Card[]) => this.pickRandomCards(cards, amount))
     );
   }
 
   shuffleEvents(): Observable<Card[]> {
     const events$ = this.dataService.events().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, this.configuration.options.events)
+      map((cards: Card[]) => this.pickRandomCards(cards, this.configuration.options.events)
     ));
 
     return iif(() => this.configuration.options.events > 0, events$, of([]));
@@ -42,7 +43,7 @@ export class ShuffleService {
   shuffleLandmarks(): Observable<Card[]> {
     const landmarks$ = this.dataService.landmarks().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, this.configuration.options.landmarks)
+      map((cards: Card[]) => this.pickRandomCards(cards, this.configuration.options.landmarks)
     ));
 
     return iif(() => this.configuration.options.landmarks > 0, landmarks$, of([]));
@@ -51,7 +52,7 @@ export class ShuffleService {
   shuffleBoons(): Observable<Card[]> {
     const boons$ = this.dataService.boons().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, this.configuration.options.boons)
+      map((cards: Card[]) => this.pickRandomCards(cards, this.configuration.options.boons)
     ));
 
     return iif(() => this.configuration.options.boons > 0, boons$, of([]));
@@ -60,7 +61,7 @@ export class ShuffleService {
   shuffleHexes(): Observable<Card[]> {
     const hexes$ = this.dataService.hexes().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, this.configuration.options.hexes)
+      map((cards: Card[]) => this.pickRandomCards(cards, this.configuration.options.hexes)
     ));
 
     return iif(() => this.configuration.options.hexes > 0, hexes$, of([]));
@@ -69,18 +70,32 @@ export class ShuffleService {
   shuffleStates(): Observable<Card[]> {
     const states$ = this.dataService.states().pipe(
       map((cards: Card[]) => this.filterByExpansions(cards)),
-      map((cards: Card[]) => this.pickRandom(cards, this.configuration.options.states)
+      map((cards: Card[]) => this.pickRandomCards(cards, this.configuration.options.states)
     ));
 
     return iif(() => this.configuration.options.states > 0, states$, of([]));
   }
 
-  pickRandom(cards: Card[], amount: number): Card[] {
-    const pickedCardIds = math.pickRandom(cards.map((card: Card) => card.id), amount) as any as number[];
-    return pickedCardIds.map((cardId: number) => cards.find((card: Card) => card.id === cardId));
+  private pickRandomCards(cards: Card[], amount: number): Card[] {
+    const cardIds = cards.map((card: Card) => card.id);
+    const cardsAggregatedByCost = cards.reduce<Map<number,number>>(
+      (aggregation: Map<number,number>, card: Card) => {
+        aggregation.set(card.cost, aggregation.has(card.cost) ? aggregation.get(card.cost) + 1 : 1);
+        return aggregation;
+      },
+      new Map<number, number>()
+    );
+    const cardWeights = cards.map((card: Card) =>
+      this.configuration.costDistribution.get(card.cost) / cardsAggregatedByCost.get(card.cost)
+    );
+
+    const pickedCardIds = this.mathJsService.pickRandom(cardIds, amount, cardWeights) as number[];
+    const pickedCards = pickedCardIds.map((cardId: number) => cards.find((card: Card) => card.id === cardId));
+
+    return pickedCards;
   }
 
-  filterByExpansions(cards: Card[]): Card[] {
+  private filterByExpansions(cards: Card[]): Card[] {
     return cards.filter((card: Card) =>
       this.configuration.expansions.map((expansion: Expansion) => expansion.id).includes(card.expansion.id)
     );
