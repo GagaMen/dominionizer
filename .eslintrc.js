21657323c8519258ef63b2421e82e13e5ad53738
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-03-01 20:07:07 +0100

    Specify .eslintrc.js as root configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    root: true,
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends:  [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
