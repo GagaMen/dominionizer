5cfd592844d15e3098ca105c992918ff34837754
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-03-24 21:46:36 +0100

    Update to Angular 9
    Co-authored-by: Felix Dimmel <12294922+GagaMen@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatSidenav } from '@angular/material/sidenav';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    @ViewChild(MatSidenav, { static: false }) private sidenav: MatSidenav;
+    @ViewChild(MatSidenav) private sidenav: MatSidenav;
 
     title = 'dominionizer';
 
