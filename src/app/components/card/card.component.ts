133d720199588dcbe133855519015b0ab3b3e1e3
GagaMen <12294922+GagaMen@users.noreply.github.com>
2020-06-25 21:53:32 +0200

    Add name of card types to result page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../../models/card';
+import { CardType } from 'src/app/models/card-type';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
     @Input() card: Card | null = null;
+    types: string[] = [];
+
+    ngOnInit(): void {
+        if (this.card?.types !== undefined) {
+            this.types = this.card?.types?.map<string>((type: CardType) => CardType[type]);
+        }
+    }
 }
