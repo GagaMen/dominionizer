3a262d4116f25bfd7c6e9ab8f63c65c84c906f1d
GagaMen <12294922+GagaMen@users.noreply.github.com>
2020-07-23 21:27:02 +0200

    Update AppBarConfiguration in ConfigurationComponent
    Co-authored-by: Tobias Dimmel <2879214+TobiDimmel@users.noreply.github.com>

--- src/app/components/configuration/configuration.component.spec.ts
+++ src/app/components/configuration/configuration.component.spec.ts
@@ -1,24 +1,71 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NEVER } from 'rxjs';
+import { SpyObj } from './../../../testing/spy-obj';
+import { AppBarConfiguration } from './../../models/app-bar-configuration';
+import { DataFixture } from './../../../testing/data-fixture';
+import { AppBarService } from './../../services/app-bar.service';
+import { ShuffleService } from './../../services/shuffle.service';
+import { Router } from '@angular/router';
+import { ConfigurationService } from './../../services/configuration.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfigurationComponent } from './configuration.component';
 
-xdescribe('ConfigurationComponent', () => {
+describe('ConfigurationComponent', () => {
     let component: ConfigurationComponent;
     let fixture: ComponentFixture<ConfigurationComponent>;
-
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [ConfigurationComponent],
-        }).compileComponents();
-    }));
+    let configurationServiceSpy: SpyObj<ConfigurationService>;
+    let shuffleServiceSpy: SpyObj<ShuffleService>;
+    let appBarServiceSpy: SpyObj<AppBarService>;
+    let dataFixture: DataFixture;
 
     beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ConfigurationComponent],
+            providers: [
+                {
+                    provide: Router,
+                    useValue: jasmine.createSpyObj<Router>('Router', ['navigate']),
+                },
+                {
+                    provide: ConfigurationService,
+                    useValue: {},
+                },
+                {
+                    provide: ShuffleService,
+                    useValue: {},
+                },
+                {
+                    provide: AppBarService,
+                    useValue: jasmine.createSpyObj<AppBarService>('AppBarService', [
+                        'updateConfiguration',
+                    ]),
+                },
+            ],
+        });
+
+        dataFixture = new DataFixture();
+
+        configurationServiceSpy = TestBed.inject(ConfigurationService) as jasmine.SpyObj<
+            ConfigurationService
+        >;
+        configurationServiceSpy.configuration$ = NEVER;
+        shuffleServiceSpy = TestBed.inject(ShuffleService) as jasmine.SpyObj<ShuffleService>;
+        appBarServiceSpy = TestBed.inject(AppBarService) as jasmine.SpyObj<AppBarService>;
+
         fixture = TestBed.createComponent(ConfigurationComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    describe('ngOnInit', () => {
+        it('should update AppBarConfiguration correctly', () => {
+            const configuration: AppBarConfiguration = {
+                navigationAction: 'none',
+                actions: [],
+            };
+
+            fixture.detectChanges();
+
+            expect(appBarServiceSpy.updateConfiguration).toHaveBeenCalledWith(configuration);
+        });
     });
 });
--- src/app/components/configuration/configuration.component.ts
+++ src/app/components/configuration/configuration.component.ts
@@ -1,6 +1,7 @@
+import { AppBarService } from './../../services/app-bar.service';
 import { ShuffleService } from '../../services/shuffle.service';
 import { ConfigurationService } from '../../services/configuration.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Router } from '@angular/router';
 import { Configuration } from '../../models/configuration';
@@ -11,15 +12,23 @@ import { Configuration } from '../../models/configuration';
     styleUrls: ['./configuration.component.scss'],
     providers: [{ provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true } }],
 })
-export class ConfigurationComponent {
+export class ConfigurationComponent implements OnInit {
     constructor(
         private router: Router,
         private configurationService: ConfigurationService,
         private shuffleService: ShuffleService,
+        private appBarService: AppBarService,
     ) {
         this.configurationService.configuration$.subscribe(
             (configuration: Configuration) => (this.shuffleService.configuration = configuration),
         );
+    }
+
+    ngOnInit(): void {
+        this.appBarService.updateConfiguration({
+            navigationAction: 'none',
+            actions: [],
+        });
     }
 
     onSubmit(): void {
