83701f71c4e9c3b1c273541e9c312be78061e285
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-10-13 21:47:56 +0200

    Use recommended configs of @angular/eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,72 +3,25 @@ module.exports = {
     overrides: [
         {
             files: ['*.ts'],
-            plugins: ['@angular-eslint'],
             extends: [
                 'eslint:recommended',
                 'plugin:@typescript-eslint/eslint-recommended',
                 'plugin:@typescript-eslint/recommended',
                 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+                'plugin:@angular-eslint/recommended',
                 'plugin:prettier/recommended',
                 'prettier/@typescript-eslint',
             ],
-            parserOptions: {
-                ecmaVersion: 2018,
-                project: './tsconfig.base.json',
-                tsconfigRootDir: __dirname,
-            },
             rules: {
-                '@angular-eslint/component-class-suffix': 'error',
                 '@angular-eslint/component-selector': [
                     'error',
                     { type: 'element', prefix: 'app', style: 'kebab-case' },
                 ],
-                '@angular-eslint/contextual-lifecycle': 'error',
-                // '@angular-eslint/directive-class-suffix': 'error',
                 '@angular-eslint/directive-selector': [
                     'error',
                     { type: 'attribute', prefix: 'app', style: 'camelCase' },
                 ],
-                '@typescript-eslint/interface-name-prefix': 'off',
-                '@typescript-eslint/member-ordering': [
-                    'error',
-                    {
-                        default: [
-                            'static-field',
-                            'instance-field',
-                            'static-method',
-                            'instance-method',
-                        ],
-                    },
-                ],
-                '@angular-eslint/no-conflicting-lifecycle': 'error',
                 '@typescript-eslint/no-floating-promises': 'off',
-                '@angular-eslint/no-host-metadata-property': 'error',
-                '@angular-eslint/no-input-rename': 'error',
-                '@angular-eslint/no-inputs-metadata-property': 'error',
-                '@angular-eslint/no-output-native': 'error',
-                '@angular-eslint/no-output-on-prefix': 'error',
-                '@angular-eslint/no-output-rename': 'error',
-                '@angular-eslint/no-outputs-metadata-property': 'error',
-                'no-restricted-imports': [
-                    'error',
-                    {
-                        paths: [
-                            {
-                                name: 'rxjs/Rx',
-                                message: "Please import directly from 'rxjs' instead",
-                            },
-                        ],
-                    },
-                ],
-                'no-restricted-syntax': [
-                    'error',
-                    {
-                        selector:
-                            'CallExpression[callee.object.name="console"][callee.property.name=/^(debug|info|time|timeEnd|trace)$/]',
-                        message: 'Unexpected property on console object was called',
-                    },
-                ],
                 '@typescript-eslint/no-unsafe-assignment': 'off',
                 '@typescript-eslint/no-unsafe-call': 'off',
                 '@typescript-eslint/no-unsafe-member-access': 'off',
@@ -78,15 +31,13 @@ module.exports = {
                         ignoreStatic: true,
                     },
                 ],
-                '@angular-eslint/use-lifecycle-interface': 'warn',
-                '@angular-eslint/use-pipe-transform-interface': 'error',
             },
         },
         {
             files: ['*.js'],
             extends: ['eslint:recommended', 'plugin:prettier/recommended'],
             parserOptions: {
-                ecmaVersion: 2018,
+                ecmaVersion: 2020,
             },
             env: {
                 node: true,
@@ -98,13 +49,7 @@ module.exports = {
         },
         {
             files: ['*.component.html'],
-            parser: '@angular-eslint/template-parser',
-            plugins: ['@angular-eslint/template'],
-            extends: ['plugin:prettier/recommended'],
-            rules: {
-                '@angular-eslint/template/banana-in-a-box': 'error',
-                '@angular-eslint/template/no-negated-async': 'error',
-            },
+            extends: ['plugin:@angular-eslint/template/recommended', 'plugin:prettier/recommended'],
         },
     ],
 };
