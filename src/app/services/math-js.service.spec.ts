b9367f13b51ecd43a96b4adf48d8b0cf0c0eeba5
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-10-27 19:59:16 +0100

    Add tests for MathJsService
    - inject math.MathJsStatic in constructor of MathJsService

--- src/app/services/math-js.service.spec.ts
+++ src/app/services/math-js.service.spec.ts
@@ -1,12 +1,86 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MathJsService } from './math-js.service';
+import { MathJsService, MathJsStaticInjectionToken } from './math-js.service';
+import { DataFixture } from 'src/testing/data-fixture';
+import * as math from 'mathjs';
+import { SpyObj } from 'src/testing/spy-obj';
+
+describe('MathJsStaticInjectionToken', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [{ provide: MathJsStaticInjectionToken }],
+        });
+    });
+
+    it('should provide "math" constant', () => {
+        const actual = TestBed.inject<math.MathJsStatic>(MathJsStaticInjectionToken);
+
+        expect(actual).toBe(math);
+    });
+});
 
 describe('MathJsService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+    let mathJsService: MathJsService;
+    let mathJsStaticSpy: SpyObj<math.MathJsStatic>;
+    let dataFixture: DataFixture;
 
-    it('should be created', () => {
-        const service: MathJsService = TestBed.inject(MathJsService);
-        expect(service).toBeTruthy();
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                MathJsService,
+                {
+                    provide: MathJsStaticInjectionToken,
+                    useValue: jasmine.createSpyObj<math.MathJsStatic>('math.MathJsStatic', [
+                        'pickRandom',
+                    ]),
+                },
+            ],
+        });
+
+        dataFixture = new DataFixture();
+        mathJsStaticSpy = TestBed.inject<SpyObj<math.MathJsStatic>>(MathJsStaticInjectionToken);
+        mathJsService = TestBed.inject(MathJsService);
+    });
+
+    describe('pickRandomCards', () => {
+        it('with number is 1 should return correct cards', () => {
+            const cards = dataFixture.createCards();
+            const number = 1;
+            const expected = cards.slice(0, number);
+            mathJsStaticSpy.pickRandom
+                .withArgs(jasmine.any(Array), number)
+                .and.returnValue(cards[0].id);
+
+            const actual = mathJsService.pickRandomCards(cards, number);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('with number is more than 1 should return correct cards', () => {
+            const cards = dataFixture.createCards();
+            const number = 2;
+            const expected = cards.slice(0, number);
+            mathJsStaticSpy.pickRandom
+                .withArgs(jasmine.any(Array), number)
+                .and.returnValue([cards[0].id, cards[1].id]);
+
+            const actual = mathJsService.pickRandomCards(cards, number);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('with weights should return correct cards', () => {
+            const cards = dataFixture.createCards();
+            const number = 1;
+            const weights = [1, 3, 2];
+            const expected = cards.slice(0, number);
+            mathJsStaticSpy.pickRandom
+                .withArgs(jasmine.any(Array), number, weights)
+                .and.returnValue(cards[0].id);
+
+            const actual = mathJsService.pickRandomCards(cards, number, weights);
+
+            expect(actual).toEqual(expected);
+        });
     });
 });
--- src/app/services/math-js.service.ts
+++ src/app/services/math-js.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken, Inject } from '@angular/core';
 import * as math from 'mathjs';
 import { Card } from '../models/card';
+
+export const MathJsStaticInjectionToken = new InjectionToken<math.MathJsStatic>(
+    'math.MathJsStatic',
+    {
+        providedIn: 'root',
+        factory: () => math,
+    },
+);
 
 @Injectable({
     providedIn: 'root',
 })
 export class MathJsService {
-    pickRandomCards(cards: Card[], number?: number, weights?: number[]): Card[] {
+    constructor(@Inject(MathJsStaticInjectionToken) private mathJsService: math.MathJsStatic) {}
+
+    pickRandomCards(cards: Card[], number: number, weights?: number[]): Card[] {
         const cardIds = cards.map((card: Card) => card.id);
         const pickedCardIds = weights
-            ? math.pickRandom(cardIds, number, weights)
-            : math.pickRandom(cardIds, number);
+            ? this.mathJsService.pickRandom(cardIds, number, weights)
+            : this.mathJsService.pickRandom(cardIds, number);
 
         return typeof pickedCardIds === 'number'
             ? [cards.find((card: Card) => card.id === pickedCardIds) as Card]
