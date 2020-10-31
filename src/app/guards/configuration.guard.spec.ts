105119a9474e5b448debe2faa25a7c7e8007b620
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2020-10-31 19:07:29 +0100

    Rework ConfigurationGuard and add tests
    - use ConfigurationService instead of ShuffleService to get information about the current Configuration

--- src/app/guards/configuration.guard.spec.ts
+++ src/app/guards/configuration.guard.spec.ts
@@ -1,15 +1,70 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { ConfigurationGuard } from './configuration.guard';
+import { SpyObj } from 'src/testing/spy-obj';
+import { Router } from '@angular/router';
+import { ConfigurationService } from '../services/configuration.service';
+import { DataFixture } from 'src/testing/data-fixture';
+import { cold, getTestScheduler } from 'jasmine-marbles';
 
-xdescribe('ConfigurationGuard', () => {
+describe('ConfigurationGuard', () => {
+    let configurationGuard: ConfigurationGuard;
+    let routerSpy: SpyObj<Router>;
+    let configurationServiceSpy: SpyObj<ConfigurationService>;
+    let dataFixture: DataFixture;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [ConfigurationGuard],
+            providers: [
+                ConfigurationGuard,
+                { provide: Router, useValue: jasmine.createSpyObj<Router>('Router', ['navigate']) },
+                { provide: ConfigurationService, useValue: {} },
+            ],
         });
+
+        dataFixture = new DataFixture();
+        routerSpy = (TestBed.inject(Router) as unknown) as SpyObj<Router>;
+        configurationServiceSpy = (TestBed.inject(ConfigurationService) as unknown) as SpyObj<
+            ConfigurationService
+        >;
+        configurationGuard = TestBed.inject(ConfigurationGuard);
     });
 
-    it('should ...', inject([ConfigurationGuard], (guard: ConfigurationGuard) => {
-        expect(guard).toBeTruthy();
-    }));
+    describe('canActivate', () => {
+        it('with current configuration has any expansions activated should return true and complete', () => {
+            const configuration = dataFixture.createConfiguration({
+                expansions: dataFixture.createExpansions(),
+            });
+            configurationServiceSpy.configuration$ = cold('-a-----', { a: configuration });
+            const expected$ = cold('                       -(b|)  ', { b: true });
+
+            const actual$ = configurationGuard.canActivate();
+
+            expect(actual$).toBeObservable(expected$);
+        });
+
+        it('with current configuration has no expansions activated should return false and complete', () => {
+            const configuration = dataFixture.createConfiguration({
+                expansions: [],
+            });
+            configurationServiceSpy.configuration$ = cold('-a-----', { a: configuration });
+            const expected$ = cold('                       -(b|)  ', { b: false });
+
+            const actual$ = configurationGuard.canActivate();
+
+            expect(actual$).toBeObservable(expected$);
+        });
+
+        it('with current configuration has no expansions activated should navigate to start page', () => {
+            const configuration = dataFixture.createConfiguration({
+                expansions: [],
+            });
+            configurationServiceSpy.configuration$ = cold('-a-----', { a: configuration });
+
+            configurationGuard.canActivate().subscribe({ error: fail });
+            getTestScheduler().flush();
+
+            expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+        });
+    });
 });
--- src/app/guards/configuration.guard.ts
+++ src/app/guards/configuration.guard.ts
@@ -1,20 +1,27 @@
-import { ShuffleService } from './../services/shuffle.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, UrlTree, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ConfigurationService } from '../services/configuration.service';
+import { map, first } from 'rxjs/operators';
+import { Configuration } from '../models/configuration';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ConfigurationGuard implements CanActivate {
-    constructor(private router: Router, private shuffleService: ShuffleService) {}
+    constructor(private router: Router, private configurationService: ConfigurationService) {}
 
-    canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        if (this.shuffleService.configuration.expansions.length !== 0) {
-            return true;
-        }
+    canActivate(): Observable<boolean> {
+        return this.configurationService.configuration$.pipe(
+            first(),
+            map((configuration: Configuration) => {
+                if (configuration.expansions.length !== 0) {
+                    return true;
+                }
 
-        this.router.navigate(['/']);
-        return false;
+                this.router.navigate(['/']);
+                return false;
+            }),
+        );
     }
 }
