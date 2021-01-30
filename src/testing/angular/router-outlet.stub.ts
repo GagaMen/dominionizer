ebc28cd18535809c4e5f895782a9438d2843bf33
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2021-01-30 11:59:27 +0100

    Clean up tests for AppComponent

---
+++ src/testing/angular/router-outlet.stub.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @angular-eslint/directive-selector */
+/* eslint-disable @angular-eslint/directive-class-suffix */
+import { Directive } from '@angular/core';
+
+@Directive({ selector: 'router-outlet' })
+export class RouterOutletStub {}
---
+++ src/testing/components/app-bar.stub.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-bar',
+    template: '',
+})
+export class AppBarStubComponent {}
