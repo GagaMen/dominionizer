b9ce9ff513fd850cb829dff50f0b344e01b8c959
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2021-05-22 11:51:18 +0200

    feat(wiki-bot): update CardDtoBuilder to extract debt and potion

--- tools/wiki-bot/src/wiki-bot/builder/card-dto-builder.spec.ts
+++ tools/wiki-bot/src/wiki-bot/builder/card-dto-builder.spec.ts
@@ -6,15 +6,22 @@ import { CardDto } from 'src/app/dtos/card-dto';
 describe('CardDtoBuilder', () => {
     let cardDtoBuilder: CardDtoBuilder;
 
-    const expansionMap: Map<string, number[]> = new Map();
-    expansionMap.set('Moat', [914, 914.1]);
+    const cardExpansionsMap: Map<string, number[]> = new Map();
+    cardExpansionsMap.set('Moat', [914, 914.1]);
+    cardExpansionsMap.set('Engineer', [2739]);
+    cardExpansionsMap.set('Alchemist', [176]);
+
+    const undefinedCardDto: Partial<CardDto> = {
+        potion: undefined,
+        debt: undefined,
+    };
 
     beforeEach(() => {
-        cardDtoBuilder = new CardDtoBuilder(expansionMap);
+        cardDtoBuilder = new CardDtoBuilder(cardExpansionsMap);
     });
 
     describe('build', () => {
-        it('should return correct card', () => {
+        it('with basic cardPage should return correct card', () => {
             const cardPage: CardPage = {
                 pageid: 18,
                 title: 'Moat',
@@ -27,12 +34,69 @@ describe('CardDtoBuilder', () => {
                 ],
             };
             const expected: CardDto = {
+                ...undefinedCardDto,
                 id: cardPage.pageid,
                 name: cardPage.title,
                 expansions: [914, 914.1],
                 types: [CardType.Action, CardType.Reaction],
                 isKingdomCard: true,
                 cost: 2,
+            };
+
+            const actual = cardDtoBuilder.build(cardPage);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('with cardPage contains debt should return correct card', () => {
+            const cardPage: CardPage = {
+                pageid: 3337,
+                title: 'Engineer',
+                fullurl: 'http://wiki.dominionstrategy.com/index.php/Engineer',
+                revisions: [
+                    {
+                        '*':
+                            '{{Infobox Card\\n |name = Engineer\\n |cost2 = 4\\n |type1 = Action\\n}}',
+                    },
+                ],
+            };
+            const expected: CardDto = {
+                ...undefinedCardDto,
+                id: cardPage.pageid,
+                name: cardPage.title,
+                expansions: [2739],
+                types: [CardType.Action],
+                isKingdomCard: true,
+                cost: 0,
+                debt: 4,
+            };
+
+            const actual = cardDtoBuilder.build(cardPage);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('with cardPage contains potion should return correct card', () => {
+            const cardPage: CardPage = {
+                pageid: 129,
+                title: 'Alchemist',
+                fullurl: 'http://wiki.dominionstrategy.com/index.php/Alchemist',
+                revisions: [
+                    {
+                        '*':
+                            '{{Infobox Card\\n |name = Alchemist\\n |cost = 3P\\n |type1 = Action\\n}}',
+                    },
+                ],
+            };
+            const expected: CardDto = {
+                ...undefinedCardDto,
+                id: cardPage.pageid,
+                name: cardPage.title,
+                expansions: [176],
+                types: [CardType.Action],
+                isKingdomCard: true,
+                cost: 3,
+                potion: true,
             };
 
             const actual = cardDtoBuilder.build(cardPage);
--- tools/wiki-bot/src/wiki-bot/builder/card-dto-builder.ts
+++ tools/wiki-bot/src/wiki-bot/builder/card-dto-builder.ts
@@ -3,27 +3,29 @@ import { CardPage, WikiText } from '../wiki-client/api-models';
 import { CardDto } from 'src/app/dtos/card-dto';
 
 export class CardDtoBuilder {
-    constructor(private expansionMap: Map<string, number[]>) {}
+    constructor(private cardExpansionsMap: Map<string, number[]>) {}
 
     build(cardPage: CardPage): CardDto {
+        const wikiText: WikiText = cardPage.revisions[0]['*'] ?? '';
+        const infoBox: WikiText = /\{\{Infobox Card\\n.*\}\}/g.exec(wikiText)?.[0] ?? '';
+
         return {
             id: cardPage.pageid,
             name: cardPage.title,
             expansions: this.extractExpansions(cardPage),
-            types: this.extractTypes(cardPage),
-            isKingdomCard: this.extractIsKingdomCard(cardPage),
-            cost: this.extractCost(cardPage),
+            types: this.extractTypes(infoBox),
+            isKingdomCard: this.extractIsKingdomCard(infoBox),
+            cost: this.extractCost(infoBox),
+            potion: this.extractPotion(infoBox),
+            debt: this.extractDebt(infoBox),
         };
     }
 
     private extractExpansions(cardPage: CardPage): number[] {
-        return this.expansionMap.get(cardPage.title) ?? [];
+        return this.cardExpansionsMap.get(cardPage.title) ?? [];
     }
 
-    private extractTypes(cardPage: CardPage): number[] {
-        const wikiText: WikiText = cardPage.revisions[0]['*'] ?? '';
-        const infoBox: WikiText = /\{\{Infobox Card\\n.*\}\}/g.exec(wikiText)?.[0] ?? '';
-
+    private extractTypes(infoBox: WikiText): number[] {
         const typeNames: string[] = Object.keys(CardType).filter(
             (typeName: string) => !(parseInt(typeName) >= 0),
         );
@@ -39,19 +41,27 @@ export class CardDtoBuilder {
         return types;
     }
 
-    private extractIsKingdomCard(cardPage: CardPage): boolean {
-        const wikiText: WikiText = cardPage.revisions[0]['*'] ?? '';
-        const infoBox: WikiText = /\{\{Infobox Card\\n.*\}\}/.exec(wikiText)?.[0] ?? '';
+    private extractIsKingdomCard(infoBox: WikiText): boolean {
         const isKingdomCard: WikiText = /\|kingdom.*\\n/.exec(infoBox)?.[0] ?? '';
 
         return !/= No/.test(isKingdomCard);
     }
 
-    private extractCost(cardPage: CardPage): number {
-        const wikiText: WikiText = cardPage.revisions[0]['*'] ?? '';
-        const infoBox: WikiText = /\{\{Infobox Card\\n.*\}\}/.exec(wikiText)?.[0] ?? '';
-        const cost: WikiText = /\|cost.*\\n/.exec(infoBox)?.[0] ?? '';
+    private extractCost(infoBox: WikiText): number {
+        const cost: WikiText = /\|cost =.*\\n/.exec(infoBox)?.[0] ?? '';
 
         return Number(/\d+/.exec(cost)?.[0] ?? 0);
+    }
+
+    private extractPotion(infoBox: WikiText): boolean | undefined {
+        const cost: WikiText = /\|cost =.*\\n/.exec(infoBox)?.[0] ?? '';
+
+        return /P/.test(cost) ? true : undefined;
+    }
+
+    private extractDebt(infoBox: WikiText): number | undefined {
+        const cost2: WikiText = /\|cost2 =.*\\n/.exec(infoBox)?.[0] ?? '';
+
+        return cost2 ? Number(/= (\d+)/.exec(cost2)?.[1]) : undefined;
     }
 }
