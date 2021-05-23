4c350b6b9381b0e0a1be95bc9d477990d21d0840
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2021-05-23 19:13:56 +0200

    feat(wiki-bot): add ExpansionTranslationBuilder

---
+++ tools/wiki-bot/src/wiki-bot/builder/expansion-translation-builder.spec.ts
@@ -0,0 +1,71 @@
+import { ExpansionTranslation } from 'src/app/models/expansion';
+import { ExpansionPage } from '../wiki-client/api-models';
+import { ExpansionTranslationBuilder } from './expansion-translation-builder';
+
+describe('ExpansionTranslationBuilder', () => {
+    let expansionTranslationBuilder: ExpansionTranslationBuilder;
+
+    beforeEach(() => {
+        expansionTranslationBuilder = new ExpansionTranslationBuilder();
+    });
+
+    describe('build', () => {
+        it('should return correct translations', () => {
+            const expansionPage: ExpansionPage = {
+                pageid: 914,
+                title: 'Dominion (Base Set)',
+                revisions: [
+                    {
+                        '*':
+                            `=== In other languages ===\\n` +
+                            `* Chinese: 皇舆争霸 (pron. ''huáng yú zhēngbà'', lit. ''Struggle for the emperor's throne'')\\n` +
+                            `* Czech: Dominion\\n` +
+                            `* Dutch: \\n` +
+                            `** Dominion: In naam van de Koning! (lit. ''In the name of the King!'') (first edition)\\n` +
+                            `** Dominion: Wie bouwt het mooiste koninkrijk? (lit. ''Who will build the most beautiful kingdom?'') (first edition)\\n` +
+                            `** Dominion: Tweede editie (second edition)\\n` +
+                            `* Finnish: Dominion: Valtakunta (lit. ''The kingdom'')\\n` +
+                            `* French: Dominion: Votre royaume commence ici! (lit. ''Your kingdom begins here!'')\\n` +
+                            `* German: Dominion: Was für eine Welt! (lit. ''What a world!'')\\n` +
+                            `* Greek: Dominion: Ο κυρίαρχος (pron. ''o kyriarchos'', lit. ''The dominant'')\\n` +
+                            `* Hungarian: Dominion\\n` +
+                            `* Italian: Dominion: Nasce un Regno (lit. ''Birth of a kingdom'')\\n` +
+                            `* Japanese: ドミニオン (pron. ''dominion'')\\n` +
+                            `* Korean: 도미니언 (pron. ''dominieon'')\\n` +
+                            `* Norwegian: Dominion\\n` +
+                            `* Polish: \\n` +
+                            `** Dominion: Rozdarte Królestwo (lit. ''A kingdom torn apart'') ([[Bard Centrum Gier|Bard]] edition)\\n` +
+                            `** Dominion ([[GFP]] edition)\\n` +
+                            `* Romanian: Dominion\\n` +
+                            `* Russian: Доминион (pron. ''dominion'')\\n` +
+                            `* Spanish: Dominion\\n` +
+                            `* Swedish: Dominion: Spelet om kungadömet (lit. ''The game of the kingdom'')\\n\\n`,
+                    },
+                ],
+            };
+            const expected: Map<string, ExpansionTranslation> = new Map([
+                ['Chinese', { id: expansionPage.pageid, name: '皇舆争霸' }],
+                ['Czech', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Dutch', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Finnish', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['French', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['German', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Greek', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Hungarian', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Italian', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Japanese', { id: expansionPage.pageid, name: 'ドミニオン' }],
+                ['Korean', { id: expansionPage.pageid, name: '도미니언' }],
+                ['Norwegian', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Polish', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Romanian', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Russian', { id: expansionPage.pageid, name: 'Доминион' }],
+                ['Spanish', { id: expansionPage.pageid, name: 'Dominion' }],
+                ['Swedish', { id: expansionPage.pageid, name: 'Dominion' }],
+            ]);
+
+            const actual = expansionTranslationBuilder.build(expansionPage);
+
+            expect(actual).toEqual(expected);
+        });
+    });
+});
---
+++ tools/wiki-bot/src/wiki-bot/builder/expansion-translation-builder.ts
@@ -0,0 +1,22 @@
+import { ExpansionTranslation } from 'src/app/models/expansion';
+import { ExpansionPage, WikiText } from '../wiki-client/api-models';
+
+export class ExpansionTranslationBuilder {
+    build(expansionPage: ExpansionPage): Map<string, ExpansionTranslation> {
+        const wikiText: WikiText = expansionPage.revisions[0]['*'] ?? '';
+        const inOtherLanguages: WikiText =
+            /=== In other languages ===\\n\* ([^=]*)/.exec(wikiText)?.[1] ?? '';
+        const languages: WikiText = inOtherLanguages.split(/\\n\* /);
+
+        return new Map<string, ExpansionTranslation>(
+            languages.map((language: WikiText) => {
+                const extractedLanguage: string = /^\w*/.exec(language)?.[0] ?? '';
+                const name: WikiText =
+                    language.split(/\\n\*\* /)[1] ?? /^\w*: (.*)/.exec(language)?.[1] ?? '';
+                const extractedName: string = /[^(:]*/.exec(name)?.[0].trim() ?? '';
+
+                return [extractedLanguage, { id: expansionPage.pageid, name: extractedName }];
+            }),
+        );
+    }
+}
