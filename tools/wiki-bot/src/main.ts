ec33aac490bcf9a1fdb82a474d341447f0e3fef2
GagaMen <12294922+GagaMen@users.noreply.github.com>
2022-07-04 23:03:59 +0200

    fix(wiki-bot): add missing parameters in main.ts
    Co-authored-by: Tobias Dimmel<2879214+TobiDimmel@users.noreply.github.com>

--- tools/wiki-bot/src/main.ts
+++ tools/wiki-bot/src/main.ts
@@ -1,11 +1,36 @@
+import { ExpansionCardsMapBuilder } from './wiki-bot/builder/expansion-cards-map-builder';
+import { ImagePool } from '@squoosh/lib';
+import { ImageBuilder } from './wiki-bot/builder/image-builder';
+import { CardTranslationBuilder } from './wiki-bot/builder/card-translation-builder';
+import { CardDtoBuilder } from './wiki-bot/builder/card-dto-builder';
 import { ExpansionTranslationBuilder } from './wiki-bot/builder/expansion-translation-builder';
 import { ExpansionBuilder } from './wiki-bot/builder/expansion-builder';
 import axios from 'axios';
 import { DominionizerWikiBot } from './wiki-bot/dominionizer-wiki-bot';
 import { WikiClient } from './wiki-bot/wiki-client/wiki-client';
 
-const axiosInstance = axios.create({});
+const axiosInstance = axios.create({
+    baseURL: 'http://wiki.dominionstrategy.com/api.php',
+    timeout: 60 * 1000,
+});
+const imagePool = new ImagePool();
+
+const targetPath = '../../../src/assets/';
 const wikiClient = new WikiClient(axiosInstance);
 const expansionBuilder = new ExpansionBuilder();
 const expansionTranlationBuilder = new ExpansionTranslationBuilder();
-const bot = new DominionizerWikiBot(wikiClient, expansionBuilder, expansionTranlationBuilder);
+const expansionCardsMapBuilder = new ExpansionCardsMapBuilder();
+const cardDtoBuilder = new CardDtoBuilder();
+const cardTranslationBuilder = new CardTranslationBuilder();
+const imageBuilder = new ImageBuilder(wikiClient, imagePool);
+
+const bot = new DominionizerWikiBot(
+    targetPath,
+    wikiClient,
+    expansionBuilder,
+    expansionTranlationBuilder,
+    expansionCardsMapBuilder,
+    cardDtoBuilder,
+    cardTranslationBuilder,
+    imageBuilder,
+);
