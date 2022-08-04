07fec8557eeb708222f7f4b320ca7e547638fb1f
GagaMen <12294922+GagaMen@users.noreply.github.com>
2022-08-04 21:27:29 +0200

    fix(wiki-bot): fix missing translation for card types with language versions

--- tools/wiki-bot/src/wiki-bot/builder/card-type-translation-builder.spec.ts
+++ tools/wiki-bot/src/wiki-bot/builder/card-type-translation-builder.spec.ts
@@ -12,33 +12,71 @@ describe('CardTypeTranslationBuilder', () => {
     describe('build', () => {
         it('should return correct translations', () => {
             const cardTypePage: CardTypePage = {
-                pageid: 216,
-                title: 'Action',
-                fullurl: 'http://wiki.dominionstrategy.com/index.php/Knight',
+                pageid: 431,
+                title: 'Prize',
+                fullurl: 'http://wiki.dominionstrategy.com/index.php/Prize',
                 revisions: [
                     {
                         '*':
                             `=== In other languages ===\n` +
-                            `* Czech: Akce\n` +
-                            `* Dutch: Actie\n` +
-                            `* Finnish: Toiminta\n` +
-                            `* French: Action\n` +
-                            `* German: Aktion\n` +
-                            `* Italian: Azione\n` +
-                            `* Polish: Akcja\n` +
-                            `* Russian: Действие (pron. ''dyeystviye'')`,
+                            `* Czech: Odměna\n` +
+                            `* Dutch: Prijs\n` +
+                            `* Finnish: Palkinto\n` +
+                            `* German: Preis\n` +
+                            `* Polish: Nagroda\n` +
+                            `* Russian: Трофей (pron. ''trofyey'', lit. ''trophy'')`,
                     },
                 ],
             };
             const expected: Map<string, CardTypeTranslation> = new Map([
-                ['Czech', { id: cardTypePage.pageid, name: 'Akce' }],
-                ['Dutch', { id: cardTypePage.pageid, name: 'Actie' }],
-                ['Finnish', { id: cardTypePage.pageid, name: 'Toiminta' }],
-                ['French', { id: cardTypePage.pageid, name: 'Action' }],
-                ['German', { id: cardTypePage.pageid, name: 'Aktion' }],
-                ['Italian', { id: cardTypePage.pageid, name: 'Azione' }],
-                ['Polish', { id: cardTypePage.pageid, name: 'Akcja' }],
-                ['Russian', { id: cardTypePage.pageid, name: 'Действие' }],
+                ['Czech', { id: cardTypePage.pageid, name: 'Odměna' }],
+                ['Dutch', { id: cardTypePage.pageid, name: 'Prijs' }],
+                ['Finnish', { id: cardTypePage.pageid, name: 'Palkinto' }],
+                ['German', { id: cardTypePage.pageid, name: 'Preis' }],
+                ['Polish', { id: cardTypePage.pageid, name: 'Nagroda' }],
+                ['Russian', { id: cardTypePage.pageid, name: 'Трофей' }],
+            ]);
+
+            const actual = cardTypeTranslationBuilder.build(cardTypePage);
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('with card type page contains versions should return correct translations', () => {
+            const cardTypePage: CardTypePage = {
+                pageid: 577,
+                title: 'Knight',
+                fullurl: 'http://wiki.dominionstrategy.com/index.php/Knight',
+                revisions: [
+                    {
+                        '*':
+                            `===Other language versions===\n` +
+                            `In general, the specific names of the Knights do not change in different languages.\n` +
+                            `{| class="wikitable" style="text-align:center;"\n! Language !! Name !! Print !! Digital !! Text !! Notes\n|-\n` +
+                            `!Czech\n| Rytíři || {{CardVersionImage|KnightsCzech|Czech language Knights}} || || Před každou hrou, v niž se používají řytíři, zamíchej jejich balíček.  Polož ho lícem dolů s výjimkou vrchní karty. Tu jedinou je možné ziskat. || \n|-\n` +
+                            `!Dutch\n| Ridders || || || || \n|-\n` +
+                            `!Finnish\n| Ritarit || || || || \n|-\n` +
+                            `!French\n| Chevalier || || || || \n|-\n` +
+                            `!German\n| Ritter || {{CardVersionImage|Knights German-HiG|German language Knights 2019 by ASS}} || || Spielvorbereitung:<br>Mischt alle Ritter, legt sie als verdeckten Stapel in den Vorrat und deckt die oberste Karte auf. Es darf immer nur die oberste Karte des Stapels genommen oder gekauft werden. || (2019)\n|-\n` +
+                            `!Japanese \n| 騎士 (pron. ''kishi'')  || || || style=\"padding:15px 0px;\"| 騎士を使用する各ゲ一ムの開始前に、騎士の山をシャッフルする。一番上のカード以外は裏にする。山の一番上のカードのみ購入•獲得可能。|| \n|-\n` +
+                            `!Korean\n| 기사 (pron. ''gisa'') || || || || \n|-\n` +
+                            `!Polish\n| Rycerze || || || || <small>Although Polish version is not released,<br>this name is referred to in Polish Dominion 2E rulebook.</small>\n|-\n` +
+                            `!Russian\n| Рыцари (pron. ''rytsari'') || || || || \n|-\n` +
+                            `!Spanish\n|Caballero || ||  || \n|}`,
+                    },
+                ],
+            };
+            const expected: Map<string, CardTypeTranslation> = new Map([
+                ['Czech', { id: cardTypePage.pageid, name: 'Rytíři' }],
+                ['Dutch', { id: cardTypePage.pageid, name: 'Ridders' }],
+                ['Finnish', { id: cardTypePage.pageid, name: 'Ritarit' }],
+                ['French', { id: cardTypePage.pageid, name: 'Chevalier' }],
+                ['German', { id: cardTypePage.pageid, name: 'Ritter' }],
+                ['Japanese', { id: cardTypePage.pageid, name: '騎士' }],
+                ['Korean', { id: cardTypePage.pageid, name: '기사' }],
+                ['Polish', { id: cardTypePage.pageid, name: 'Rycerze' }],
+                ['Russian', { id: cardTypePage.pageid, name: 'Рыцари' }],
+                ['Spanish', { id: cardTypePage.pageid, name: 'Caballero' }],
             ]);
 
             const actual = cardTypeTranslationBuilder.build(cardTypePage);
--- tools/wiki-bot/src/wiki-bot/builder/card-type-translation-builder.ts
+++ tools/wiki-bot/src/wiki-bot/builder/card-type-translation-builder.ts
@@ -6,18 +6,52 @@ export class CardTypeTranslationBuilder {
     build(cardTypePage: CardTypePage): Map<string, CardTypeTranslation> {
         const wikiText: WikiText = cardTypePage.revisions[0]['*'] ?? '';
         const inOtherLanguages: WikiText = extractSection(wikiText, 'In other languages', 3);
-        const languageCandidates: WikiText[] = inOtherLanguages.split(/\n\*\s/).slice(1);
+
+        if (inOtherLanguages !== '') {
+            const languageCandidates: WikiText[] = inOtherLanguages.split(/\n\*\s/).slice(1);
+            return new Map<string, CardTypeTranslation>(
+                languageCandidates.map((languageCandidate: WikiText) => {
+                    const language = /^[^:]*/.exec(languageCandidate)?.[0];
+                    let name: string | undefined =
+                        languageCandidate.split(/\n\*\*/)[1] ??
+                        /^[^:]*:(.*)/.exec(languageCandidate)?.[1] ??
+                        '';
+                    name = /[^(:]*/.exec(name)?.[0];
+
+                    return [
+                        normalize(language),
+                        { id: cardTypePage.pageid, name: normalize(name) },
+                    ];
+                }),
+            );
+        }
+
+        const otherLanguageVersions: WikiText = extractSection(
+            wikiText,
+            'Other language versions',
+            3,
+        );
+        const table = /{\|(.*?)\|}/s.exec(otherLanguageVersions)?.[1] ?? '';
+        const rows = table
+            .split('|-')
+            .slice(1)
+            .filter((row) => normalize(row) !== '');
 
         return new Map<string, CardTypeTranslation>(
-            languageCandidates.map((languageCandidate: WikiText) => {
-                const language = /^[^:]*/.exec(languageCandidate)?.[0];
-                let name: string | undefined =
-                    languageCandidate.split(/\n\*\*/)[1] ??
-                    /^[^:]*:(.*)/.exec(languageCandidate)?.[1] ??
-                    '';
-                name = /[^(:]*/.exec(name)?.[0];
+            rows.map((row) => {
+                const match = /!([^|]*)\|(.*)/s.exec(row);
+                const language = normalize(match?.[1]);
+                const columns = match?.[2].split('||');
 
-                return [normalize(language), { id: cardTypePage.pageid, name: normalize(name) }];
+                const cardTypeName = normalize(columns?.[0].match(/^[^(]*/)?.[0]);
+
+                return [
+                    language,
+                    {
+                        id: cardTypePage.pageid,
+                        name: cardTypeName,
+                    },
+                ];
             }),
         );
     }
