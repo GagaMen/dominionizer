2a43d2e465a5cd316b866a71a6e38cf052f7313b
GagaMen <12294922+GagaMen@users.noreply.github.com>
2022-10-20 09:22:24 +0200

    test(components): add missing environment test for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
+import { environment } from 'src/environments/environment';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterOutletStub } from 'src/testing/angular/router-outlet.stub';
 import { AppBarStubComponent } from 'src/testing/components/app-bar.stub.component';
 import { FooterStubComponent } from 'src/testing/components/footer.stub.component';
 import { AppComponent } from './app.component';
+import { LOCALE_ID } from '@angular/core';
 
 describe('AppComponent', () => {
     let component: AppComponent;
@@ -16,6 +18,12 @@ describe('AppComponent', () => {
                 RouterOutletStub,
                 FooterStubComponent,
             ],
+            providers: [
+                {
+                    provide: LOCALE_ID,
+                    useValue: 'de',
+                },
+            ],
         });
 
         fixture = TestBed.createComponent(AppComponent);
@@ -24,5 +32,12 @@ describe('AppComponent', () => {
 
     it('should be creatable', () => {
         expect(component).toBeInstanceOf(AppComponent);
+    });
+
+    it('with production environment should set entryPoint correctly', () => {
+        environment.production = true;
+        fixture = TestBed.createComponent(AppComponent);
+
+        expect(environment.entryPoint).toBe('/de');
     });
 });
