52d5a03e7b56b13630cfe4a492bb31c59832c03a
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2023-01-29 20:59:16 +0100

    fix(wiki-bot): remove chalk
    - chalk doesn't work in Github actions

--- tools/wiki-bot/src/wiki-bot/dominionizer-wiki-bot.ts
+++ tools/wiki-bot/src/wiki-bot/dominionizer-wiki-bot.ts
@@ -28,7 +28,6 @@ import {
 import { CardType, CardTypeTranslation } from 'src/app/models/card-type';
 import { mkdir } from 'fs/promises';
 import { ValidationResult } from './validation/validation-result';
-import chalk from 'chalk';
 import { SplitPileDependencyBuilder } from './builder/split-pile-dependency-builder';
 
 export class DominionizerWikiBot {
@@ -451,7 +450,7 @@ export class DominionizerWikiBot {
             return;
         }
 
-        console.error(chalk.red(validationResult.failureMessage ?? ''));
+        console.error(validationResult.failureMessage);
         this.successful = false;
     }
 }
