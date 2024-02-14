6d97d6a8e40772a6e0f896cc370c6e55e6faa330
Tobi Dimmel <2879214+TobiDimmel@users.noreply.github.com>
2024-02-14 22:03:02 +0100

    chore(dominiontabs-converter): remove obsolete tool

--- tools/dominiontabs-converter/index.mjs
+++
@@ -1,9 +0,0 @@
-import { DominiontabsConverter } from './dominiontabs-converter.mjs';
-
-const dominiontabsConverter = new DominiontabsConverter(
-    'https://raw.githubusercontent.com/sumpfork/dominiontabs/master/card_db_src/cards_db.json',
-    './dist',
-);
-
-dominiontabsConverter.run();
-console.log('Card data successfully converted!');
